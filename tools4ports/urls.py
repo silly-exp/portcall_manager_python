72ad39892f3ef728e21cd6b847cc24c7c81114d8
Nicolas Viel <le.nicolas.viel@gmail.com>
2019-09-03 22:54:34 +0200

    première tentative de gestion des langues

--- tools4ports/urls.py
+++ tools4ports/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('callsmanager/', include('callsmanager.urls'))
 ]
+urlpatterns += i18n_patterns(
+    path('callsmanager/', include('callsmanager.urls'))
+    )
